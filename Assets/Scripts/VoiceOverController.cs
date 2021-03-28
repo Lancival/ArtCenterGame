3cc13a01e88dd95d270e515c25b7c1b6f580ad7b
Richard Cheng <rhocheng@g.ucla.edu>
2021-03-28 02:38:26 -0700

    Voice Over and Title Background

---
+++ Assets/Scripts/VoiceOverController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceOverController : MonoBehaviour
+{
+
+	private float startTime;
+	[SerializeField] private SceneLoader sl;
+
+    void Start()
+    {
+		startTime = Time.time;
+    }
+
+    void Update()
+    {
+		if (Time.time - startTime > 15.5f) 
+			sl.LoadNextScene();
+    }
+}
